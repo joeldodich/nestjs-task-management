9ee18fa4ebd651f0797c0ce8ca7c1e6533775349
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2024-05-08 15:23:14 -0400

    add pipes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty, IsString } from "class-validator";
 import { Task } from "../task.model";
-
 export class CreateTaskDto {
+    @IsNotEmpty()
+    @IsString()
     title: Task['title'];
+
+    @IsNotEmpty()
+    @IsString()
     description: Task['description'];
 }
+
