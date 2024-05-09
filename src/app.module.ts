5bff34435660684e7aeaa46d6fd4f40808994638
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2024-05-08 20:18:42 -0400

    init auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, MongooseModule.forRoot('mongodb+srv://joeledodich:kv8c6IKAUjC8adlx@cluster0.81zgsu7.mongodb.net/')],
+  imports: [TasksModule, MongooseModule.forRoot('mongodb+srv://joeledodich:kv8c6IKAUjC8adlx@cluster0.81zgsu7.mongodb.net/'), AuthModule],
 })
 export class AppModule {}
