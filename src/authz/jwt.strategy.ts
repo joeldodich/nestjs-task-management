20a02fd544a5f6d5da513cd7e9d57701554ac96a
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2024-05-09 10:23:44 -0400

    save

--- src/authz/jwt.strategy.ts
+++ src/authz/jwt.strategy.ts
@@ -1,11 +1,10 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { passportJwtSecret } from "jwks-rsa";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { passportJwtSecret } from 'jwks-rsa';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  
   constructor() {
     super({
       secretOrKeyProvider: passportJwtSecret({
@@ -21,9 +20,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       algorithms: ['RS256'],
     });
   }
-  
 
-  validate(payload: unknown): unknown {
+  validate(payload: JwtPayload): JwtPayload {
     return payload;
   }
+}
+
+interface JwtPayload {
+  iss: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  gty: string;
+  azp: string;
 }
