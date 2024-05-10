fb42c45d67c0291482ddb85b156483d4cf1f6a53
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2024-05-10 16:59:56 -0400

    tasks live on orgs

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,7 +1,9 @@
 import { Document } from "mongoose";
+import { Organization } from "src/organizations/organization.model";
 
 export interface Task extends Document {
   _id: string;
+  owner: Organization['_id'];
   title: string;
   description: string;
   status: TaskStatus;
